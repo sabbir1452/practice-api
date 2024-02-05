0b4163050bb1504f2b3375d362113142d8578d35
sabbir1452 <sabbirrahman1452@gmail.com>
2024-02-05 06:56:25 +0600

    so many times commited

---
+++ practice.js
@@ -0,0 +1,42 @@
+/* 
+1. arrow function 
+2. map, forEach, filter, find
+3. template string
+4. API--fetch
+5. DOM manipulation (recap)
+*/
+
+/* 
+PRACTICE-1
+1. load comments and display comments on the UI
+2. display 20 comments only (first 20)
+3. add a click handler on the comment:
+4. if you click on the comment, it will open the relevant POST details
+https://jsonplaceholder.typicode.com/posts/${postId}
+*/
+
+/* PRACTICE-2 
+1. load data for a random user
+2. display image
+3.city, street,
+*/
+
+/* PRACTICE-3
+ practice mealdb again
+---------------
+*/
+
+/*
+PRACTICE-4
+1. explore sports db
+2. display something
+3. dynamic API url
+4. add handler to display something by loading data
+
+*/
+
+/* 
+PRACTICE-5
+1. explore network tab
+2. display preview
+*/
